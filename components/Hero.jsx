e26c3e1167ca0473fb728d2478934dda98df2c6b
czDamian <olebueziedamian@gmail.com>
2025-03-07 13:59:41 +0100

    ui

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,42 +2,45 @@ import Image from "next/image";
 import Link from "next/link";
 import Benefits from "./Benefits";
 import Footer from "./Footer";
+import StatsCard from "./StatsCard";
 
 const Hero = () => {
   return (
-    <section className=" bg-light-pink-50">
-      <div className="bg-[#BD99A2] w-full ">
+    <section className="bg-light-pink-50">
+      <div className="bg-[#BD99A2] w-full">
         <h1 className="bg-[#EFE6EA] text-xl sm:text-2xl md:text-3xl font-coolvetica text-center py-4 px-12 mx-16 tracking-wider">
           Agent Bryan
         </h1>
       </div>
-      <div className="relative px-4">
-        <div className="text-3xl md:text-4xl mb-96 flex gap-3 flex-col text-[#d180ac] mt-16 font-coolvetica md:font-semibold">
-          <div>Unlock Your</div>
-          <div>Health Potential</div>
-          <div>With AI-Powered</div>
-          <div>Insights</div>
+
+      {/* New div wrapping the heading and hero-brain image */}
+      <div
+        className="relative w-full bg-cover bg-[position:left_15%,right_60%] md:bg-[position:left_15%,right_70%] lg:bg-[position:left_15%,right_80%]"
+        style={{
+          backgroundImage: `url('/agent-transparent.png'), url('/bryan-transparent.png')`,
+          backgroundRepeat: "no-repeat, no-repeat",
+          backgroundSize: "contain, contain",
+        }}
+      >
+        <div className="relative px-4 py-6">
+          <div className="text-3xl md:text-4xl mb-96 flex gap-3 flex-col text-[#d180ac] font-coolvetica md:font-semibold">
+            <div>Unlock Your</div>
+            <div>Health Potential</div>
+            <div>With AI-Powered</div>
+            <div>Insights</div>
+          </div>
+          <div>
+            <Image
+              src="/hero-brain.png"
+              height={1000}
+              width={1000}
+              alt="hero-brain"
+              className="w-80 absolute top-20 right-[-5]"
+            />
+          </div>
         </div>
-        <div>
-          <Image
-            src="/hero-brain.png"
-            height={1000}
-            width={1000}
-            alt="hero-brain"
-            className="w-80 absolute  top-20 right-[-5]"
-          />
-        </div>
-        {/* <div>
-          <Image
-            src="/hero-bg-2.png"
-            height={1000}
-            width={1000}
-            alt="hero-brain"
-            className="w-80 absolute top-20 right-[-5] z-50 hidden md:block"
-          />
-        </div> */}
       </div>
-      <div className="text-center py-16 ">
+      <div className="text-center py-16">
         <Link
           href="/onboard"
           className="bg-[#8F788D] hover:bg-[#7e6a7c] text-lg text-white px-20 py-3 rounded-full"
@@ -45,8 +48,10 @@ const Hero = () => {
           Get Started
         </Link>
       </div>
+      <StatsCard />
+
       <div className="bg-[#BE98A2] text-white px-4">
-        <div className="text-4xl p-8  text-center">
+        <div className="text-4xl p-8 text-center">
           <div className="mt-12">Experience the</div>
           <div>benefit of using</div>
           <div>Agent Bryan</div>
@@ -56,7 +61,7 @@ const Hero = () => {
           tailored insights, real-time security alerts, and interactive learning
           with earning opportunities. Its intuitive design makes crypto
           education engaging and rewarding, helping you make smarter, safer
-          decisionsi
+          decisions.
         </p>
         <Benefits />
       </div>
---
+++ components/StatsCard.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaUser, FaDatabase, FaChartLine } from "react-icons/fa";
+
+const StatsCard = () => {
+  const statsData = [
+    {
+      id: 1,
+      icon: <FaUser size={24} />,
+      value: "100k",
+      label: "Trusted Users",
+    },
+    {
+      id: 2,
+      icon: <FaDatabase size={24} />,
+      value: "750+",
+      label: "Health Queries Processed",
+    },
+    {
+      id: 3,
+      icon: <FaChartLine size={24} />,
+      value: "95%",
+      label: "Health Insights Provided",
+    },
+  ];
+
+  return (
+    <div className="flex mx-auto max-w-xl w-max flex-col sm:flex-row gap-4 sm:gap-2 md:gap-4 py-8">
+      {statsData.map((stat) => (
+        <div
+          key={stat.id}
+          className="min-w-[200px] md:min-w-[250px]  flex-1 bg-gradient-to-b from-[#484245]  to-[#774F65] bg-opacity-80 p-6 flex flex-col items-start text-white border-r border-[#774F65] last:border-r-0"
+        >
+          <div className="mb-2 p-1 bg-white bg-opacity-10 rounded">
+            {stat.icon}
+          </div>
+          <div className="text-3xl md:text-5xl font-bold mt-1">
+            {stat.value}
+          </div>
+          <div className="mt-1">
+            <div className="text-sm">{stat.label}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default StatsCard;
