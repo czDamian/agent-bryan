8f97216e556be11e5b4ee60c45dcbc9f83971e5a
czDamian <olebueziedamian@gmail.com>
2025-03-07 15:00:37 +0100

    ui

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,9 +8,29 @@ const Hero = () => {
   return (
     <section className="bg-light-pink-50">
       <div className="bg-[#BD99A2] w-full">
-        <h1 className="bg-[#EFE6EA] text-xl sm:text-2xl md:text-3xl font-coolvetica text-center py-4 px-12 mx-16 tracking-wider">
-          Agent Bryan
-        </h1>
+        {/* desktop  view */}
+        <div className="bg-[#EFE6EA] hidden md:flex text-xl sm:text-2xl md:text-3xl font-coolvetica text-center py-4 px-12 mx-16 tracking-wider justify-between items-center">
+          <Link
+            href="/"
+            className="hidden md:inline-block font-[100] mr-4 md:ml-8 font-rubik tracking-normal text-2xl"
+          >
+            Chat
+          </Link>
+          <div className="hidden md:block">Agent Bryan</div>
+          <Link
+            href="/"
+            className="hidden md:inline-block font-[100] md:mr-8 ml-4 font-rubik tracking-normal text-2xl border border-light-pink-100 px-4 py-1 rounded-lg hover:bg-light-pink-100 hover:text-white"
+          >
+            Get Started
+          </Link>
+        </div>
+        <div>
+          {/* for mobile view */}
+
+          <div className="bg-[#EFE6EA] text-xl sm:text-2xl md:text-3xl font-coolvetica text-center py-4 px-12 mx-16 tracking-wider md:hidden ">
+            Agent Bryan
+          </div>
+        </div>
       </div>
 
       {/* New div wrapping the heading and hero-brain image */}
@@ -50,19 +70,22 @@ const Hero = () => {
       </div>
       <StatsCard />
 
-      <div className="bg-[#BE98A2] text-white px-4">
-        <div className="text-4xl p-8 text-center">
-          <div className="mt-12">Experience the</div>
-          <div>benefit of using</div>
-          <div>Agent Bryan</div>
+      <div className="bg-[#BE98A2] text-white px-4 ">
+        <div clas="flex flex-1 flex-col md:flex-row justify-center items-center">
+          <div className="text-4xl lg:text-5xl space-y-4 p-4">
+            <div className="mt-12">Experience the</div>
+            <div>benefit of using</div>
+            <div>Agent Bryan</div>
+          </div>
+          <p className=" mt-8 md:mt-16 max-w-md md:text-lg text-center p-8">
+            Brayann AI delivers a seamless, personalized crypto experience. Get
+            tailored insights, real-time security alerts, and interactive
+            learning with earning opportunities. Its intuitive design makes
+            crypto education engaging and rewarding, helping you make smarter,
+            safer decisions.
+          </p>
         </div>
-        <p className="mx-auto mt-8 max-w-md text-center">
-          Brayann AI delivers a seamless, personalized crypto experience. Get
-          tailored insights, real-time security alerts, and interactive learning
-          with earning opportunities. Its intuitive design makes crypto
-          education engaging and rewarding, helping you make smarter, safer
-          decisions.
-        </p>
+
         <Benefits />
       </div>
       <Footer />
